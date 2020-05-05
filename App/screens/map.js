4f33fa29a5e7b6b93d265ceca7ac532a6ff29a49
Noortje Veenhuizen <noortjeveenhuizen@hotmail.com>
2020-05-05 13:44:17 +0200

    geolocation map done

--- App/screens/map.js
+++ App/screens/map.js
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import MapView from 'react-native-maps';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
+
+const { width, height } = Dimensions.get('window');
+
+const SCREEN_HEIGHT = height;
+const SCREEN_WIDTH = width;
+const ASPECT_RATIO = width / height;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 
 const styles = StyleSheet.create({
   radius: {
@@ -36,32 +44,78 @@ const styles = StyleSheet.create({
   },
 });
 
-const Map = () => {
-  return (
-    <View style={styles.container}>
-      <MapView
-        style={styles.mapStyle}
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-        mapType={'satellite'}
-      >
-        <MapView.Marker
-          coordinate={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-          }}
-        >
-          <View style={styles.radius}>
-            <View style={styles.marker} />
-          </View>
-        </MapView.Marker>
-      </MapView>
-    </View>
-  );
-};
+export default class geolocationMap extends Component {
+  constructor(props) {
+    super(props);
 
-export default Map;
+    this.state = {
+      initialPosition: {
+        latitude: 0,
+        longitude: 0,
+        latitudeDelta: 0,
+        longitudeDelta: 0,
+      },
+      markerPosition: {
+        latitude: 0,
+        longitude: 0,
+      },
+    };
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        var lat = parseFloat(position.coords.latitude);
+        var long = parseFloat(position.coords.longitude);
+
+        var initialRegion = {
+          latitude: lat,
+          longitude: long,
+          latitudeDelta: LATITUDE_DELTA,
+          longitudeDelta: LONGITUDE_DELTA,
+        };
+
+        this.setState({ initialPosition: initialRegion });
+        this.setState({ markerPosition: initialRegion });
+      },
+      (error) => alert(JSON.stringify(error)),
+      { enableHighAccuracy: true, timeout: 2000, maximumAge: 1000 }
+    );
+
+    this.watchID = navigator.geolocation.watchPosition((position) => {
+      var lat = parseFloat(position.coords.latitude);
+      var long = parseFloat(position.coords.longitude);
+
+      var lastRegion = {
+        latitude: lat,
+        longitude: long,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA,
+      };
+
+      this.setState({ initialPosition: lastRegion });
+      this.setState({ markerPosition: lastRegion });
+    });
+  }
+  componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.watchID);
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <MapView
+          style={styles.mapStyle}
+          region={this.state.initialPosition}
+          mapType={'satellite'}
+        >
+          <MapView.Marker coordinate={this.state.markerPosition}>
+            <View style={styles.radius}>
+              <View style={styles.marker} />
+            </View>
+          </MapView.Marker>
+        </MapView>
+      </View>
+    );
+  }
+}
