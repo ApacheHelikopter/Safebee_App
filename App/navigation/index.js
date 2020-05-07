5511f83d028fd07dcca884efe15ef0ffa623ee14
ApacheHelikopter <pietersruben@hotmail.com>
2020-05-07 20:42:27 +0200

    implemented auth flow with awesome login screens :)))

--- App/navigation/index.js
+++ App/navigation/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { createSwitchNavigator, createAppContainer } from 'react-navigation';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
 import Icon from 'react-native-vector-icons/MaterialIcons';
@@ -7,6 +8,11 @@ import MaterialIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 import connectHesjes from '../screens/connectHesjes';
 import connect from '../screens/connect';
 import QRScanner from '../screens/QRScanner';
+
+//Auth imports
+import Login from '../screens/Auth/loginScreen';
+import Register from '../screens/Auth/registerScreen';
+import LoadingScreen from '../screens/Auth/loadingScreen';
 
 //Map imports
 import Map from '../screens/map';
@@ -17,7 +23,6 @@ import SettingsTab from '../screens/Settings/settings';
 import FAQ from '../screens/Settings/FAQ';
 import Help from '../screens/Settings/help';
 import Meldingen from '../screens/meldingen';
-import Login from '../screens/Settings/login';
 
 //Profiel imports
 import ProfielTab from '../screens/Profile/profile';
@@ -26,12 +31,46 @@ import ConnectHesje from '../screens/Profile/connectHesje';
 import DisconnectHesje from '../screens/Profile/disconnectHesje';
 
 //Radius imports
-import Radius from '../screens/Radius/radiusSheet';
+
+import * as firebase from 'firebase';
+
+let firebaseConfig = {
+  apiKey: 'AIzaSyC4S1VsYyUp1cFGyNgDjTrQZ0uuLiOC-Q4',
+  authDomain: 'safebee-4f297.firebaseapp.com',
+  databaseURL: 'https://safebee-4f297.firebaseio.com',
+  projectId: 'safebee-4f297',
+  storageBucket: 'safebee-4f297.appspot.com',
+  messagingSenderId: '357730359210',
+  appId: '1:357730359210:web:6399581ebf2be8eeb9bdd3',
+  measurementId: 'G-182CQNE80E',
+};
+
+//initialize FireBase
+firebase.initializeApp(firebaseConfig);
 
 const Tab = createBottomTabNavigator();
 
+const AppStack = createStackNavigator();
+const AuthStack = createStackNavigator();
 const SettingsStack = createStackNavigator();
 const ProfileStack = createStackNavigator();
+
+const AppStackScreen = () => {
+  return (
+    <AppStack.Navigator>
+      <AppStack.Screen name="Home" component={TabNavigator} />
+    </AppStack.Navigator>
+  );
+};
+
+const AuthStackScreen = () => {
+  return (
+    <AuthStack.Navigator>
+      <AuthStack.Screen name="Login" component={Login} />
+      <AuthStack.Screen name="Register" component={Register} />
+    </AuthStack.Navigator>
+  );
+};
 
 const SettingsStackScreen = () => {
   return (
@@ -85,48 +124,57 @@ const ProfileStackScreen = () => {
   );
 };
 
-class TabNavigator extends React.Component {
-  render() {
-    return (
-      <NavigationContainer>
-        <Tab.Navigator
-          tabBarOptions={{
-            showLabel: false,
-            activeTintColor: '#F6C004',
-            inactiveTintColor: 'gray',
-          }}
-        >
-          <Tab.Screen
-            name="Home"
-            component={GeoLocationMap}
-            options={{
-              tabBarIcon: ({ color }) => (
-                <Icon name="location-on" size={30} color={color} />
-              ),
-            }}
-          />
-          <Tab.Screen
-            name="Profiel"
-            component={ProfileStackScreen}
-            options={{
-              tabBarIcon: ({ color }) => (
-                <Icon name="person" size={35} color={color} />
-              ),
-            }}
-          />
-          <Tab.Screen
-            name="Instellingen"
-            component={SettingsStackScreen}
-            options={{
-              tabBarIcon: ({ color }) => (
-                <MaterialIcon name="dots-horizontal" size={35} color={color} />
-              ),
-            }}
-          />
-        </Tab.Navigator>
-      </NavigationContainer>
-    );
-  }
-}
+const TabNavigator = () => {
+  return (
+    <Tab.Navigator
+      tabBarOptions={{
+        showLabel: false,
+        activeTintColor: '#F6C004',
+        inactiveTintColor: 'gray',
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={GeoLocationMap}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Icon name="location-on" size={30} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profiel"
+        component={ProfileStackScreen}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Icon name="person" size={35} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Instellingen"
+        component={SettingsStackScreen}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <MaterialIcon name="dots-horizontal" size={35} color={color} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
 
-export default TabNavigator;
+const SafebeeApp = createAppContainer(
+  createSwitchNavigator(
+    {
+      Loading: LoadingScreen,
+      App: TabNavigator,
+      Auth: AuthStackScreen,
+    },
+    {
+      initialRouteName: 'Loading',
+    }
+  )
+);
+
+export default SafebeeApp;
