da624afab5fa323c0a189983e54b41d072612064
Noortje Veenhuizen <noortjeveenhuizen@hotmail.com>
2020-05-07 14:31:38 +0200

    basic text login

--- App/screens/Settings/login.js
+++ App/screens/Settings/login.js
@@ -1,14 +1,81 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
+import {
+  Text,
+  View,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
 
 const Login = () => {
   return (
-    <View>
-      <Button title="Meldingen"></Button>
-      <Button title="FAQ"></Button>
-      <Button title="Help"></Button>
+    <View style={StyleSheet.ViewContainer}>
+      <TextInput
+        style={styles.inputText}
+        placeholder="Email..."
+        placeholderTextColor="#003f5c"
+        onChangeText={(text) => this.setState({ email: text })}
+      />
+
+      <View style={styles.inputView}>
+        <TextInput
+          secureTextEntry
+          style={styles.inputText}
+          placeholder="Password..."
+          placeholderTextColor="#003f5c"
+          onChangeText={(text) => this.setState({ password: text })}
+        />
+      </View>
+
+      <TouchableOpacity>
+        <Text style={styles.forgot}>Forgot Password?</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.loginBtn}>
+        <Text style={styles.loginText}>LOGIN</Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Text style={styles.loginText}>Signup</Text>
+      </TouchableOpacity>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  viewContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputView: {
+    width: '80%',
+    backgroundColor: '#465881',
+    borderRadius: 25,
+    height: 50,
+    marginBottom: 20,
+    justifyContent: 'center',
+    padding: 20,
+  },
+  inputText: {
+    height: 50,
+    color: 'white',
+  },
+  forgot: {
+    color: 'white',
+    fontSize: 11,
+  },
+  loginBtn: {
+    width: '80%',
+    backgroundColor: '#fb5b5a',
+    borderRadius: 25,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 40,
+    marginBottom: 10,
+  },
+  loginText: {
+    color: 'white',
+  },
+});
 
 export default Login;
