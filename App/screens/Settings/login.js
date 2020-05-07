7109a475ac051eaa3ee0c56ec455e946b08a9ee0
Noortje Veenhuizen <noortjeveenhuizen@hotmail.com>
2020-05-07 14:53:38 +0200

    little changes login fields

--- App/screens/Settings/login.js
+++ App/screens/Settings/login.js
@@ -10,31 +10,33 @@ import {
 const Login = () => {
   return (
     <View style={StyleSheet.ViewContainer}>
-      <TextInput
-        style={styles.inputText}
-        placeholder="Email..."
-        placeholderTextColor="#003f5c"
-        onChangeText={(text) => this.setState({ email: text })}
-      />
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.inputText}
+          placeholder="E-mailadres"
+          placeholderTextColor="#9F9F9F"
+          onChangeText={(text) => this.setState({ email: text })}
+        />
+      </View>
 
       <View style={styles.inputView}>
         <TextInput
           secureTextEntry
           style={styles.inputText}
-          placeholder="Password..."
-          placeholderTextColor="#003f5c"
+          placeholder="Wachtwoord"
+          placeholderTextColor="#9F9F9F"
           onChangeText={(text) => this.setState({ password: text })}
         />
       </View>
 
       <TouchableOpacity>
-        <Text style={styles.forgot}>Forgot Password?</Text>
+        <Text style={styles.forgot}>Wachtwoord vergeten?</Text>
       </TouchableOpacity>
       <TouchableOpacity style={styles.loginBtn}>
-        <Text style={styles.loginText}>LOGIN</Text>
+        <Text style={styles.loginText}>Inloggen</Text>
       </TouchableOpacity>
       <TouchableOpacity>
-        <Text style={styles.loginText}>Signup</Text>
+        <Text style={styles.loginText}>nog geen lid? registreer dan nu</Text>
       </TouchableOpacity>
     </View>
   );
@@ -45,10 +47,11 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#FFFFFF',
   },
   inputView: {
     width: '80%',
-    backgroundColor: '#465881',
+    backgroundColor: '#F8F8F8',
     borderRadius: 25,
     height: 50,
     marginBottom: 20,
@@ -60,13 +63,20 @@ const styles = StyleSheet.create({
     color: 'white',
   },
   forgot: {
-    color: 'white',
-    fontSize: 11,
+    color: '#F6C004',
+    fontSize: 12,
   },
   loginBtn: {
     width: '80%',
-    backgroundColor: '#fb5b5a',
+    backgroundColor: '#ffffff',
     borderRadius: 25,
+    shadowColor: '#000000',
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    shadowOffset: {
+      height: 1,
+      width: 1,
+    },
     height: 50,
     alignItems: 'center',
     justifyContent: 'center',
@@ -74,7 +84,7 @@ const styles = StyleSheet.create({
     marginBottom: 10,
   },
   loginText: {
-    color: 'white',
+    color: '#F6C004',
   },
 });
 
