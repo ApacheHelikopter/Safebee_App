e28b58fd21a71b75dbd7c38215feaab91c2add74
Noortje Veenhuizen <noortjeveenhuizen@hotmail.com>
2020-05-15 10:39:08 +0200

    background color profile

--- App/screens/Profile/profile.js
+++ App/screens/Profile/profile.js
@@ -4,32 +4,39 @@ import CustomButton from '../../components/CustomButton/customButton';
 
 const ProfielTab = ({ navigation }) => {
   return (
-    <View style={styles.profileTab}>
-      <View>
-        <TouchableOpacity>
-          <Text>20</Text>
-        </TouchableOpacity>
-        <Text>hesjes geconnecteerd</Text>
-      </View>
-      <View style={styles.profileActions}>
-        <CustomButton
-          title="Mijn groepen"
-          onPress={() => navigation.navigate('MijnGroepen')}
-        />
-        <CustomButton
-          title="Hesjes connecteren"
-          onPress={() => navigation.navigate('ConnectHesje')}
-        />
-        <CustomButton
-          title="Hesjes afmelden"
-          onPress={() => navigation.navigate('DisconnectHesje')}
-        />
+    <View style={styles.screen}>
+      <View style={styles.profileTab}>
+        <View>
+          <TouchableOpacity>
+            <Text>20</Text>
+          </TouchableOpacity>
+          <Text>hesjes geconnecteerd</Text>
+        </View>
+        <View style={styles.profileActions}>
+          <CustomButton
+            title="Mijn groepen"
+            onPress={() => navigation.navigate('MijnGroepen')}
+          />
+          <CustomButton
+            title="Hesjes connecteren"
+            onPress={() => navigation.navigate('ConnectHesje')}
+          />
+          <CustomButton
+            title="Hesjes afmelden"
+            onPress={() => navigation.navigate('DisconnectHesje')}
+          />
+        </View>
       </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  screen: {
+    backgroundColor: 'white',
+    height: '100%',
+  },
+
   profileTab: {
     display: 'flex',
     margin: '8%',
